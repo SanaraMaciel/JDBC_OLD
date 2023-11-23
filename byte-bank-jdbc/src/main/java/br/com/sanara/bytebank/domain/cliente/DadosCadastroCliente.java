83deed98b6e4c965cabc155582d43f45b354d18e
Sanara Felicio <sanara@cpqd.com.br>
2023-11-23 16:02:37 -0300

    Curso completo de JDBC em JAVA

---
+++ byte-bank-jdbc/src/main/java/br/com/sanara/bytebank/domain/cliente/DadosCadastroCliente.java
@@ -0,0 +1,6 @@
+package br.com.sanara.bytebank.domain.cliente;
+
+
+
+public record DadosCadastroCliente(String nome, String cpf, String email) {
+}
---
+++ byte-bank-jdbc/src/main/java/br/com/sanara/bytebank/domain/conta/DadosAberturaConta.java
@@ -0,0 +1,8 @@
+package br.com.sanara.bytebank.domain.conta;
+
+import br.com.sanara.bytebank.domain.cliente.DadosCadastroCliente;
+import br.com.sanara.bytebank.domain.cliente.DadosCadastroCliente;
+
+public record DadosAberturaConta(Integer numero, DadosCadastroCliente dadosCliente) {
+
+}
